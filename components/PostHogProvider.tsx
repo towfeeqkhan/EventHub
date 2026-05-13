4fe8baab7c3cceae1d15a7f6b5271f76574a30a5
Towfeeq Khan <ask.towfeeq@gmail.com>
2026-05-14 01:29:05 +0530

    refactor: move posthog init to useEffect and add initialization check

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -23,13 +23,16 @@ function PostHogPageView() {
 }
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  if (typeof window !== "undefined") {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      capture_pageview: false,
-      capture_pageleave: true,
-    });
-  }
+  useEffect(() => {
+    if (!posthog.__loaded && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+        capture_pageview: false,
+        capture_pageleave: true,
+      });
+    }
+  }, []);
 
   return (
     <PHProvider client={posthog}>
